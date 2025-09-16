598c7f167dd4cb56ad419282b2310e123df8ff5b
pavanvikas36 <pavanvivek36@gmail.com>
2025-09-16 17:24:15 +0530

    Mern Stack Project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar({ isLoggedIn, userRole }) {
         className="flex items-center gap-3 cursor-pointer flex-1"
         onClick={() => navigate("/")}
       >
-        <img src={logo} alt="RevVolt Logo" className="h-16 w-32" />
+        <img src={logo} alt="RevVolt Logo" className="max-h-16 w-auto" />
       </div>
 
       {/* Center: Menu (only for buyers) */}
